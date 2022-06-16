702297535b4b15501aede9ee9cdd1e58ed9e63c8
Karen Li <kkl224@lehigh.edu>
2022-06-16 13:26:12 -0400

    Update

--- test.py
+++ test.py
@@ -1,10 +1,29 @@
-from pickle import TRUE
-from turtle import Turtle
+# Standard imports
 import cv2 as cv
 import numpy as np
 
 # Capturing video through webcam
 videoCapture = cv.VideoCapture(1)
+
+# Setup SimpleBlobDetector parameters.
+params = cv.SimpleBlobDetector_Params()
+
+# Change thresholds
+params.minThreshold = 100
+params.maxThreshold = 200
+
+# Filter by Area.
+params.filterByArea = True
+params.minArea = 100
+params.maxArea = 20000
+
+# Filter by Circularity
+params.filterByCircularity = True
+params.minCircularity = 0.5
+
+params.filterByColor = True
+blueLower = (35, 140, 60)
+blueUpper = (255, 255, 180)
 
 # Start a while loop
 while(1):
@@ -13,12 +32,29 @@ while(1):
     ret, frame = videoCapture.read()
     if not ret: break
 
-    params = cv.SimpleBlobDetector_Params()
+    mask = cv.inRange(frame, blueLower, blueUpper)
+    mask = cv.erode(mask, None, iterations=0)
+    mask = cv.dilate(mask, None, iterations=0)
+    frame = cv.bitwise_and(frame,frame,mask = mask)
 
-    params.filterByArea = True
-    params.minArea = 10
+    # Set up the detector with default parameters.
+    detector = cv.SimpleBlobDetector_create(params)
 
-    params.filterByCircularity = True
-    params.minCircularity = 0.5
+    # Detect blobs.
+    keypoints = detector.detect(mask)
 
-    params.filterByColor = True
+    # Draw detected blobs as red circles.
+    # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
+    im_with_keypoints = cv.drawKeypoints(mask, keypoints, np.array([]), (0,0,255), cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+    # Display the resulting frame
+    frame = cv.bitwise_and(frame,im_with_keypoints,mask = mask)
+
+    cv.imshow('frame',frame)
+
+    # Wait for Esc key to stop
+    if cv.waitKey(33) == 27:
+        # De-allocate any associated memory usage
+        cv.destroyAllWindows()
+        videoCapture.release()
+        break
