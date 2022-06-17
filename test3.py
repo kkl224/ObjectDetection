47efac2f647250867929e1a4134aa48587f40dff
Karen Li <kkl224@lehigh.edu>
2022-06-17 16:29:54 -0400

    Update

--- test3.py
+++ test3.py
@@ -3,35 +3,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Capturing video through webcam
-videoCapture = cv.VideoCapture(0)
+videoCapture = cv.VideoCapture(1)
 
 # Setup SimpleBlobDetector parameters.
 params = cv.SimpleBlobDetector_Params()
 
-# Change thresholds
-params.thresholdStep = 10
+# Change threshold
 params.minThreshold = 10
 params.maxThreshold = 220
-params.minRepeatability = 2
 
 # Filter by InertiaRatio
 params.filterByInertia = True
-params.minInertiaRatio = 0.1
+params.minInertiaRatio = 0.3
 
 # Filter by Area.
 params.filterByArea = True
 params.minArea = 200
-params.minArea = 20000
+params.maxArea = 20000
+#params.minRepeatability = 1
 
 # Filter by Convexity
 params.filterByConvexity = True
-params.minConvexity = 0.3
+params.minConvexity = 0.1
 
 # Filter by Circularity
 params.filterByCircularity = True
-params.minCircularity = 0.2
+params.minCircularity = 0.4
 
 params.filterByColor = True
+params.blobColor = 255
 
 while(1):
 
@@ -42,40 +42,30 @@ while(1):
 
     hsvframe = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
 
-    lower = np.array([60, 70, 30]) #80, 40, 30
-    upper = np.array([105, 255, 255]) 
+    lower = np.array([70, 80, 50]) #80, 40, 30
+    upper = np.array([140, 255, 255]) 
 
     mask = cv.inRange(hsvframe, lower, upper)
 
-    blue = cv.bitwise_and(frame, frame, mask=mask)
-
-    lower2 = np.array([50, 0, 0])
-    upper2 = np.array([55, 50, 50])
-
-    ## mask of blue color
-    mask2 = cv.inRange(blue, lower2, upper2)
-
-    noblue = cv.bitwise_and(blue, blue, mask2=mask2)
-
-    #print(blue.dtype, blue.shape)
-    #print(mask.dtype, mask.shape)
+    #blue = cv.bitwise_and(frame, frame, mask=mask)
 
     # Set up the detector with default parameters.
     detector = cv.SimpleBlobDetector_create(params)
 
     # Detect blobs
-    keypoints = detector.detect(blue)
+    keypoints = detector.detect(mask)
+    if len(keypoints) != 0:
+        print(len(keypoints), 'blob found!')
 
     blank = np.zeros((1, 1))
 
     # Draw detected blobs as red circles.
-    blobs = cv.drawKeypoints(blue, keypoints, blank, (0,0,255), cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    blobs = cv.drawKeypoints(frame, keypoints, blank, (0, 0, 255), cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
     plt.imshow(blobs)
     cv.imshow('Blobs', blobs)
-    cv.imshow('NoBlue', noblue)
+    cv.imshow('Mask', mask)
     #cv.imshow('Blue', blue)
-    #cv.imshow('frame', mask)
 
     # Wait for Esc key to stop
     if cv.waitKey(33) == 27:
