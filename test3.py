b16cfa259cedb2dd32eae2af50925e7e4a0d9be8
Karen Li <kkl224@lehigh.edu>
2022-06-17 14:55:33 -0400

    Update

--- test3.py
+++ test3.py
@@ -1,8 +1,37 @@
 import cv2 as cv
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Capturing video through webcam
-videoCapture = cv.VideoCapture(1)
+videoCapture = cv.VideoCapture(0)
+
+# Setup SimpleBlobDetector parameters.
+params = cv.SimpleBlobDetector_Params()
+
+# Change thresholds
+params.thresholdStep = 10
+params.minThreshold = 10
+params.maxThreshold = 220
+params.minRepeatability = 2
+
+# Filter by InertiaRatio
+params.filterByInertia = True
+params.minInertiaRatio = 0.1
+
+# Filter by Area.
+params.filterByArea = True
+params.minArea = 200
+params.minArea = 20000
+
+# Filter by Convexity
+params.filterByConvexity = True
+params.minConvexity = 0.3
+
+# Filter by Circularity
+params.filterByCircularity = True
+params.minCircularity = 0.2
+
+params.filterByColor = True
 
 while(1):
 
@@ -13,31 +42,40 @@ while(1):
 
     hsvframe = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
 
-    lower = np.array([80, 40, 40])
-    upper = np.array([130, 255, 180])
+    lower = np.array([60, 70, 30]) #80, 40, 30
+    upper = np.array([105, 255, 255]) 
 
     mask = cv.inRange(hsvframe, lower, upper)
 
     blue = cv.bitwise_and(frame, frame, mask=mask)
 
-    print(blue.dtype, blue.shape)
-    print(mask.dtype, mask.shape)
+    lower2 = np.array([50,0,0])
+    upper2 = np.array([55, 50, 50])
 
-    # Apply Hough transform on the image
-    # circles = cv.HoughCircles(mask, cv.HOUGH_GRADIENT, 1, 100, param1=10, param2=10, minRadius=1, maxRadius=300)
+    ## mask of blue color
+    mask2 = cv.inRange(blue, lower2, upper2)
 
-    #if circles is not None:
+    noblue = cv.bitwise_and(blue, blue, mask2=mask2)
 
-        # Draw circles that are detected.
-        #circles = np.uint16(np.round(circles))
+    #print(blue.dtype, blue.shape)
+    #print(mask.dtype, mask.shape)
 
-        # loop over the (x, y) coordinates and radius of the circles
-        #for i in circles[0,:]:
-            #cv.circle(frame,(i[0], i[1]), i[2], (255,0,0), 2)
-            #cv.circle(frame,(i[0], i[1]), 2, (0,0,255), 3)
-     
-    #cv.imshow('frame', blue)
-    cv.imshow('frame', mask)
+    # Set up the detector with default parameters.
+    detector = cv.SimpleBlobDetector_create(params)
+
+    # Detect blobs
+    keypoints = detector.detect(blue)
+
+    blank = np.zeros((1, 1))
+
+    # Draw detected blobs as red circles.
+    blobs = cv.drawKeypoints(blue, keypoints, blank, (0,0,255), cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+    plt.imshow(blobs)
+    cv.imshow('Blobs', blobs)
+    cv.imshow('NoBlue', noblue)
+    #cv.imshow('Blue', blue)
+    #cv.imshow('frame', mask)
 
     # Wait for Esc key to stop
     if cv.waitKey(33) == 27:
